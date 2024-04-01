30da9a8db2b801d46125e255a70b656f6a85ed52
zoidgerd <gerd.muehlberger96@gmail.com>
2024-04-01 14:26:16 +0200

    efactor constants

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import json
 import os.path
-from utility_modules.constants import cliArguments, domain
+from utility_modules.constants import CLI_ARGUMENTS, DOMAIN
 from utility_modules import factories
 from utility_modules.sentiment_anal import SentimentProviderFactory
 
 
-if os.path.isfile(f'./config/{domain}_config.json'):
+if os.path.isfile(f'./config/{DOMAIN}_config.json'):
     pass
 else:
     print(
-        f"\nCould not find config file for {domain} API client\n\n\
-          Please set up your credentials for the {domain} API client first\n")
+        f"\nCould not find config file for {DOMAIN} API client\n\nPlease set \
+          up your credentials for the {DOMAIN} API client first\n")
 
     config_setup_client = factories.get_setup_client()
     config_setup_client.set_credentials()
 
-client_config_file = json.load(open(f'./config/{domain}_config.json'))
+
+client_config_file = json.load(open(f'./config/{DOMAIN}_config.json'))
 
 
 def main():
     client = factories.get_connector().connect(config_file=client_config_file)
-    url = factories.get_url_parser().parse_url(cliArguments.url)
+    url = factories.get_url_parser().parse_url(CLI_ARGUMENTS.url)
     extractor = factories.get_extractor(client)
     raw_dataframe = extractor.fetch_raw_comments_dataframe(url)
     sentiment_provider = SentimentProviderFactory.get_provider(
-        cliArguments.sentiment_provider
+        CLI_ARGUMENTS.sentiment_provider
     )
     dataframe_with_sentiment = sentiment_provider.infer_labels_and_scores(
         raw_dataframe
--- utility_modules/constants.py
+++ utility_modules/constants.py
@@ -1,5 +1,5 @@
 from utility_modules import utils
 
-cliArguments = utils.parse_cli_arguments()
-host = utils.HostParser().parse_url(cliArguments.url)
-domain = utils.DomainParser().parse_url(cliArguments.url)
+CLI_ARGUMENTS = utils.parse_cli_arguments()
+HOST = utils.HostParser().parse_url(CLI_ARGUMENTS.url)
+DOMAIN = utils.DomainParser().parse_url(CLI_ARGUMENTS.url)
